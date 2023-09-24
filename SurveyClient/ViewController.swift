742d8c22e40b4fe09fac4334cfda2b5a01241d2c
Bryan Bolivar <bryan.andres.bolivar@gmail.com>
2023-09-24 15:02:43 -0500

    first commit, added Xcode Project

---
+++ SurveyClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SurveyClient
+//
+//  Created by Bryan A Bolivar M on 24/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
