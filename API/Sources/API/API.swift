17acfda00fce1ed2c9bdea9c3aacf9aa2d59bd4b
Bryan Bolivar <bryan.andres.bolivar@gmail.com>
2023-09-24 20:27:33 -0500

    added Moya Dependency for networking layer, added endpoints enum

--- API/Sources/API/API.swift
+++ API/Sources/API/API.swift
@@ -1,5 +1,17 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Moya
+
+public enum NimbleAPI {
+    case register(email: String, name: String, password: String, passwordConfirmation: String)
+    case loginWithEmail(email: String, password: String)
+    case refreshToken(token: String)
+    case logout
+    case forgotPassword(email: String)
+    case surveyList(page: Int)
+    case surveyDetails(surveyID: String)
+    case userProfile
+}
 
 public struct API {
     public init() {
